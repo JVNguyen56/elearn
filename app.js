6dc46ef1b30beb72f112b3ecc461492a53e3fabc
LuisMiguelRodriguez <tanukitek@gmail.com>
2017-04-18 19:04:59 -0700

    Added Middleware

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -32,6 +33,44 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Express session
+app.use(session({
+  secret: 'secret',
+  saveUnitialized: true,
+  resave:true
+}));
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormater: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length){
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg : msg,
+      value: value
+    };
+  }
+}));
+
+//connect-flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next){
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
